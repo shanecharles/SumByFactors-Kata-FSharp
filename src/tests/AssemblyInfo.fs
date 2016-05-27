ca283a448104c29a4c29edc22f763a9bb5726691
Shane Charles <shanecharles@burningicesolutions.com>
2016-05-27 16:58:37 -0500

    tests

---
+++ src/tests/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace SumByFactors.Tests
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("SumByFactors.Tests")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Burning Ice Solutions Inc.")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ src/tests/SumByFactorTests.fs
@@ -0,0 +1,26 @@
+﻿namespace TestRunner
+
+open Xunit
+open Xunit.Extensions
+open App
+
+module Tests =
+  [<Theory>]
+  [<InlineData(0)>]
+  [<InlineData(1)>]
+  [<InlineData(-1)>]
+  let ``The factors for 0, 1, and -1 should be empty.`` edge =
+    [] = factors edge
+
+  [<Theory>]
+  [<InlineData(0)>]
+  [<InlineData(1)>]
+  [<InlineData(-1)>]
+  let ``Given a sequence with 0, 1, or -1, sumByFactors should return an empty sequence.`` edge =
+    [edge] |> sumByFactors |> Seq.isEmpty
+
+  [<Fact>]
+  let ``Sequence consisting of 15, 30, and -45 should return [(2,30); (3,0); (5,0)].`` () =
+    let expected = [(2,30); (3,0); (5,0)]
+    let actual = [15; 30; -45] |> sumByFactors |> Seq.toList
+    expected = actual
